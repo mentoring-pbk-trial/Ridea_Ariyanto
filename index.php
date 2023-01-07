86740702bb30d6ef0db62141db0b19077a0f58a1
ridea88 <rideaajh88@gmail.com>
2023-01-07 10:16:01 +0700

    initial

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RIDEA ARIYANTO</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <article>
+        <h1>MENTORING PERTAMA</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum excepturi temporibus deserunt suscipit sed
+            corporis enim fuga neque voluptas sequi illo est officiis omnis placeat aut, laudantium, fugit numquam
+            ratione.
+        </p>
+        <br>
+        <button onclick="myClick()">TEST</button>
+
+    </article>
+    <script>
+        function myClick() {
+            alert('test')
+        }
+    </script>
+
+</body>
+
+</html>
